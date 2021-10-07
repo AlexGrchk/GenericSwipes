d4889a18997b4e16424042b054a8b51f6c6a6f0f
Alexander <ilold2191@gmail.com>
2021-10-07 12:23:14 +0300

    Initial Commit

---
+++ GenericSwipes/GenericSwipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericSwipesApp.swift
+//  GenericSwipes
+//
+//  Created by Alexander on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GenericSwipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
